82d422ba1abcd7ecca98199f7272b09160e8b1f4
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-01-29 18:26:30 +0000

    add prettier

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 const App = () => {
   const [message, setMessage] = useState<string>("Connecting to backend...");
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch("http://localhost:9745/", { mode: 'cors' });
-      const text = await res.text()
+      const res = await fetch("http://localhost:9745/", { mode: "cors" });
+      const text = await res.text();
       setMessage(text);
-    }
+    };
 
     const id = setInterval(fetchData, 1000);
     return () => clearInterval(id);
@@ -19,12 +19,10 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <p>Waiting for server connection&hellip;</p>
-        <p>
-          {message}
-        </p>
+        <p>{message}</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
