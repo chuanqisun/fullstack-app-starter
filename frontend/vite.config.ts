dd6bbd49765818352c19cb45187f829fb0ff1b5b
Chuanqi <chuanqi.sun.dev@gmail.com>
2024-01-30 20:26:18 -0800

    remove unused packages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  test: {
-    environment: "jsdom",
-    setupFiles: ["./tests/setup.ts"],
-  },
 });
